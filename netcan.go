70d790eb809732f1255c8f7eb804514735515ce0
Lorenzo Fontana <lo@linux.com>
2017-04-17 16:49:54 +0200

    Pushed image

--- netcan.go
+++ netcan.go
@@ -20,7 +20,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      loggedRouter,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "0.0.0.0:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
